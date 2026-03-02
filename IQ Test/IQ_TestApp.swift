fd8071a7542069d46cdf2c88ef5ea232c269faff
denysshumik <shumikdenys@gmail.com>
2026-03-02 22:24:30 +0200

    Initial Commit

---
+++ IQ Test/IQ_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  IQ_TestApp.swift
+//  IQ Test
+//
+//  Created by Denys Shumik on 02.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct IQ_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IQ TestTests/IQ_TestTests.swift
@@ -0,0 +1,17 @@
+//
+//  IQ_TestTests.swift
+//  IQ TestTests
+//
+//  Created by Denys Shumik on 02.03.2026.
+//
+
+import Testing
+@testable import IQ_Test
+
+struct IQ_TestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
